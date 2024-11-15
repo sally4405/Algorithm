a142a898470554272ab5d4537c38aed33b3cf3e0
sally4405 <sally4405@naver.com>
2024-11-15 15:52:53 +0900

    프로그래머스 - 기능개발

--- Programmers/기능개발.playground/Contents.swift
+++ Programmers/기능개발.playground/Contents.swift
@@ -22,6 +22,23 @@ func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
     return answer
 }
 
+func solution2(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    var lastDuration: Int = 0, res:[Int] = []
+
+    for i in 0..<progresses.count {
+        let progress = Double(progresses[i])
+        let speed = Double(speeds[i])
+        let day = Int((100 - progress - 1) / speed) + 1
+        if day > lastDuration {
+            lastDuration = day
+            res.append(1)
+        } else {
+            res[res.endIndex-1] += 1
+        }
+    }
+    return res
+}
+
 let progresses1 = [93, 30, 55]
 let speeds1 = [1, 30, 5]
 // [2, 1]
@@ -30,4 +47,4 @@ let progresses2 = [95, 90, 99, 99, 80, 99]
 let speeds2 = [1, 1, 1, 1, 1, 1]
 // [1, 3, 2]
 
-print(solution(progresses2, speeds2))
+print(solution2(progresses2, speeds2))
