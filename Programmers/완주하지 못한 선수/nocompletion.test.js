1fb97b2fa4ae9641e448bca9d086789a59756b82
sally4405 <sally4405@naver.com>
2024-11-15 15:54:32 +0900

    fix

--- Programmers/완주하지 못한 선수/nocompletion.test.js
+++
@@ -1,8 +0,0 @@
-const { solution } = require("./nocompletion");
-
-var part = ["marina", "josipa", "nikola", "vinko", "marina", ,"marina", "filipa"];
-var comp = ["josipa", "marina", "filipa", "marina", "nikola", "marina"];
-
-test("test", () => {
-    expect(solution(part, comp)).toBe("vinko");
-})
